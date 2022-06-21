1838609a22349eb21996a2efbcfd6b4737bfce96
Rafael Simionato <rafaasimi@hotmail.com>
2022-06-21 13:50:06 -0300

    Configuração do Apollo e criação da primeira query em GraphQL

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      slug
+      title
+      teacher {
+        name
+        bio
+        avatarURL
+      }
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
-  return <h1 className="text-2xl font-bold text-violet-500">Hello World</h1>;
+  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY);
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-violet-500">Hello World</h1>
+      {data?.lessons.map((lesson) => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </>
+  );
 }
 
 export default App;
