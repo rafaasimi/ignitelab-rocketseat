99f651239bf71f3c0b2baed9b4f84e609035cf29
Rafael Simionato <rafaasimi@hotmail.com>
2022-06-22 20:26:19 -0300

    Aula 3 - Configuração de rotas e consumindo GraphQL de acordo com a aula selecionada

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -20,9 +21,12 @@ export function Lesson(props: LessonProps) {
   );
 
   return (
-    <a href="#">
-      <span className="text-gray-300">{availableDateFormatted}</span>
-      <div className="rounded border border-gray-500 p-4 mt-2">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
+      <span className="text-gray-300">
+        {availableDateFormatted}
+      </span>
+
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
@@ -41,6 +45,6 @@ export function Lesson(props: LessonProps) {
         </header>
         <strong className="text-gray-200 mt-5 block">{props.title}</strong>
       </div>
-    </a>
+    </Link>
   );
 }
