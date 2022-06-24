3590b57222060a45ddd883036c5fe29b666d0e72
Rafael Simionato Zorzenon <rafael_zorzenon@sicredi.com.br>
2022-06-24 17:20:11 -0300

    Aula 4 - Criado pagina de inscrição e mutation para se inscrever

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4ng6ts402sv01xycg6w1sa6/master?query=query%20%7B%0A%20%20lessons%20%7B%0A%20%20%20%20id%0A%20%20%20%20slug%0A%20%20%20%20title%0A%20%20%20%20teacher%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%20%20bio%0A%20%20%20%20%20%20avatarURL%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}` 
+  },
   cache: new InMemoryCache(),
 });
